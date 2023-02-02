1edb9dd96b79d3d0e11c64074a7bf9c1214a8d24
danielr3122 <drosenhamer@gmail.com>
2023-02-02 13:26:32 +0000

    Day 1 Solution

--- day1.java
+++ day1.java
@@ -1,19 +1,11 @@
+// Advent of Code
+// Day 1, 2022
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class day1{
-    // private static int findMax(ArrayList<Integer> arr){
-    //     int max = arr.get(0);
-    //     for(int i = 0; i < arr.size() - 1; i++){
-    //         if(arr.get(i) < arr.get(i+1)){
-    //             max = arr.get(i+1);
-    //         }
-    //     }
-    //     return max;
-    // }
-
     public static void main(String[] args) throws FileNotFoundException{
         File file = new File("input1.txt");
         Scanner s = new Scanner(file);
